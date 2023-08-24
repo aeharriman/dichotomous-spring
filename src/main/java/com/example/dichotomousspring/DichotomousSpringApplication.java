23ec6195d6166a17b32d3ec7349d1f72d8f13aba
Austyn Harriman <aeharrima@gmail.com>
2023-08-24 12:39:11 -0500

    first commit

---
+++ src/main/java/com/example/dichotomousspring/DichotomousSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.dichotomousspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DichotomousSpringApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DichotomousSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dichotomousspring/DichotomousSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.dichotomousspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DichotomousSpringApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}

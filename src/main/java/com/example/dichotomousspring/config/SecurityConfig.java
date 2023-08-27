3f5c348c0c7daca30b4cd56fbeb83f6a391f8a51
Austyn Harriman <aeharrima@gmail.com>
2023-08-26 23:09:34 -0500

    cors filter and security config change. trying to troubleshoot cors error

--- src/main/java/com/example/dichotomousspring/config/SecurityConfig.java
+++ src/main/java/com/example/dichotomousspring/config/SecurityConfig.java
@@ -1,28 +1,28 @@
-//package com.example.dichotomousspring.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                // .antMatchers and mvcMatchers were both deprecated
-//                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//                .requestMatchers(HttpMethod.GET, "/api/keys").authenticated()
-//                .anyRequest().permitAll()
-//                .and()
-//                .cors(Customizer.withDefaults())
-//                .oauth2ResourceServer(Customizer.withDefaults());
-//        return http.build();
-//    }
-//
-////    Over Spring Boot 2.7 so can skip the custom JWT decoder because it supports audience validation out of the box
-//}
+package com.example.dichotomousspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                // .antMatchers and mvcMatchers were both deprecated
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/keys").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .cors(Customizer.withDefaults())
+                .oauth2ResourceServer(Customizer.withDefaults());
+        return http.build();
+    }
+
+//    Over Spring Boot 2.7 so can skip the custom JWT decoder because it supports audience validation out of the box
+}
---
+++ src/main/java/com/example/dichotomousspring/config/SimpleCORSFilter.java
@@ -0,0 +1,27 @@
+package com.example.dichotomousspring.config;
+
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        chain.doFilter(req, res);
+    }
+
+}
